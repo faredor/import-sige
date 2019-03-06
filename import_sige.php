845ca6996fb4af0421acff31edbb4e35f5323cdb
Alexander Knapp <alexander.knapp@stud.fh-campuswien.ac.at>
2019-03-06 20:09:26 +0100

    use of proper english debugging messages

--- import_sige.php
+++ import_sige.php
@@ -1,18 +1,18 @@
 <?php
    /*
    Plugin Name: Import SIGE
-   Plugin URI: http://my-awesomeness-emporium.com
-   Description: a plugin to create awesomeness and spread joy
-   Version: 1.2
-   Author: Mr. Awesome
-   Author URI: http://mrtotallyawesome.com
+   Plugin URI: https://github.com/alkn87
+   Description: Imports pictures with SIGE markup from Joomla
+   Version: 1.1
+   Author: Alexander Knapp
+   Author URI: https://github.com/alkn87
    License: GPL2
    */
 
 add_action('admin_menu', 'test_button_menu');
 
 function test_button_menu(){
-  add_menu_page('Test Button Page', 'Test Button', 'manage_options', 'test-button-slug', 'test_button_admin_page');
+  add_menu_page('Import SIGE', 'Start Import', 'manage_options', 'test-button-slug', 'test_button_admin_page');
 
 }
 
@@ -62,7 +62,8 @@ function get_files_in_dir($url){
 		$images = array();
 		/* Das ist der korrekte Weg, ein Verzeichnis zu durchlaufen. */
 		while (false !== ($file = readdir($handle))) {
-			if ($file != "." && $file != "..") {
+      $extension = pathinfo($file);
+			if ($file != "." && $file != ".." && ($extension['extension'] == "jpg" || $extension['extension'] == "png")  ) {
 				$images[] = $file;
 			}
 
@@ -103,28 +104,83 @@ function insert_img_to_wp($filename, $is_thumbnail, $parent_post_id) {
 		$count = $wpdb->get_var($query);
 
  		if ( !$count ) {
-			echo 'es passiert etwas';
+			echo 'working';
+      /*
 			// Insert the attachment.
-			$attach_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
-			echo '<h1>Bild erfolgreich hochgeladen: '.$filename.'</h1>';
-			// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
-			require_once( ABSPATH . 'wp-admin/includes/image.php' );
+      $attach_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
+			echo '<h1>Bild erfolgreich hochgeladen: '.$filename.'</h1><br>';
 
+      if (!is_wp_error($attach_id)) {
+      // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+			require_once( ABSPATH . 'wp-admin/includes/image.php' );
+      require_once( ABSPATH . 'wp-admin/includes/file.php' );
+      require_once( ABSPATH . 'wp-admin/includes/media.php' );
+      echo "laeuft<br>";
 			// Generate the metadata for the attachment, and update the database record.
 			$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
-			wp_update_attachment_metadata( $attach_id, $attach_data );
+      echo "generate meta laeuft<br>";
+			$attach_metadata = wp_update_attachment_metadata( $attach_id, $attach_data );
+      echo "update meta laeuft<br>";
+  			if ($is_thumbnail) {
+  				$thumb_data = set_post_thumbnail( $parent_post_id, $attach_id );
+          echo "set thumb laeuft<br>";
+  			}
 
-			if ($is_thumbnail) {
-				set_post_thumbnail( $parent_post_id, $attach_id );
-			}
+      }*/
+
+              // Need to require these files
+              if ( !function_exists('media_handle_upload') ) {
+              require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+              require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+              require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+              }
+
+              $url = $filename;
+              $tmp = download_url( $url );
+              if( is_wp_error( $tmp ) ){
+              // download failed, handle error
+              }
+              $post_id = $parent_post_id;
+              $desc = "";
+              $file_array = array();
+
+              // Set variables for storage
+              // fix file filename for query strings
+              preg_match('/[^\?]+\.(jpg|jpe|jpeg|gif|png)/i', $url, $matches);
+              $file_array['name'] = basename($matches[0]);
+              $file_array['tmp_name'] = $tmp;
+
+              // If error storing temporarily, unlink
+              if ( is_wp_error( $tmp ) ) {
+              @unlink($file_array['tmp_name']);
+              $file_array['tmp_name'] = '';
+              }
+
+              // do the validation and storage stuff
+              $id = media_handle_sideload( $file_array, $parent_post_id, $desc );
+
+              // If error storing permanently, unlink
+              if ( is_wp_error($id) ) {
+              @unlink($file_array['tmp_name']);
+              return $id;
+              echo "irgendein Fehler";
+              }
+
+              $src = wp_get_attachment_url( $id );
+
+              if ($is_thumbnail) {
+        				$thumb_data = set_post_thumbnail( $parent_post_id, $id );
+                echo "set thumb laeuft<br>";
+        			}
+
 		}
+
 	}
 
 
-function test_button_action()
-{
+function test_button_action() {
   echo '<div id="message" class="updated fade"><p>'
-    .'The "Call Function" button was clicked.' . '</p></div>';
+    .'import started' . '</p></div>';
 
   //$path = WP_TEMP_DIR . '/test-button-log.txt';
 
@@ -155,17 +211,20 @@ function test_button_action()
 	foreach ( $latest_posts as $post ) {
 		setup_postdata( $post );
         //echo "<h2><a href=" + the_permalink() + ">" + the_title() + "</a></h2>";
-        the_content();
+        //the_content();
 		$id = $post->ID;
 		echo $id;
-		$str = apply_filters( 'the_content', get_the_content() );
+		//$str = apply_filters( 'the_content', get_the_content() );
+    $str = apply_filters('the_content', $post->post_content);
+    //echo $str;
+
 		preg_match_all('@\{gallery\}([^,]*?)(?:,single=([^,{]+).*?)?\{/gallery\}@',$str,$out);
 
 		// Matches array:
 		//var_export($out);
 		//$out = array_unique($out);
 
-		echo "Ergebnis Ordner und Einzelbild:<br>";
+		echo "Result folder and thumbnail picture:<br>";
 
 		// Path + Image files array:
 		foreach(array_unique($out[2]) as $i=>$v){
@@ -185,7 +244,7 @@ function test_button_action()
 			}
 		}
 
-		echo "Ergebnis Ordner:<br>";
+		echo "Result folder:<br>";
 
 		// Folders only array:
 		$folders_only = array_unique(array_filter(array_slice($out,1)[0],'strlen'));
@@ -202,15 +261,18 @@ function test_button_action()
 					insert_img_to_wp($img_to_upload, 0, $id);
 				}
 			}
-		}
-    /*
+
 		// Update post
 		if (!empty($result) OR !empty($folders_only)) {
+
+
 			$searchpattern_single = '/\{gallery\}(.*?),single=(.*?){\/gallery\}/';
 			$searchpattern_normal = '/\{gallery\}(.*?){\/gallery\}/';
 			$new_str = preg_replace($searchpattern_single, "", $str);
-			$final_str = preg_replace($searchpattern_normal, '[gallery link="file"]', $new_str);
-			$my_post = array(
+			$newer_str = preg_replace($searchpattern_normal, '', $new_str);
+
+      $final_str = $newer_str.'<br>[gallery link="file"]';
+      $my_post = array(
 				'ID'	=> $id,
 				'post_content'	=> $final_str
 			);
@@ -218,12 +280,14 @@ function test_button_action()
 			// Update the post into the database
 			wp_update_post( $my_post );
 		}
-    */
+
 		echo '<hr>';
-		$out = "";
-		$result = "";
+		unset($out);
+		unset($result);
 
 	}
+}
+echo "DONE!";
 
 }
 ?>
